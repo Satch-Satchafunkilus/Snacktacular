177da624af95703e3d93a11e5161b78fa35d8039
Tushar Munge <63478179+Satch-Satchafunkilus@users.noreply.github.com>
2025-08-20 21:01:10 -0500

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Tushar Munge on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
